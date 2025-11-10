57e73e1a92a97943b25e0fa9e580cd47d955b190
Athanasios Lazios <athanasioslazios0@gmail.com>
2025-11-10 15:00:15 +0100

    Dritter Upload (Vereinsbeitrag C#)

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Vereinsbeitrag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berechnen Sie Ihren Vereinsbeitrag.");
+            Console.WriteLine("Wie alt sind Sie?");
+            int alter = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (alter <= 6)
+            {
+                Console.WriteLine("Beitrag: frei");
+            }
+            else if (alter >= 7 && alter <= 17)
+            {
+                Console.WriteLine("Beitrag: 30 Euro");
+            }
+            else if (alter >= 18 && alter <= 65)
+            {
+                string erwerbstätig = "";
+
+                while (erwerbstätig != "j" && erwerbstätig != "n")
+                {
+                    Console.WriteLine("Sind Sie erwerbstätig? (j/n)");
+                    erwerbstätig = Console.ReadLine().ToLower();
+
+                    if (erwerbstätig == "j")
+                    {
+                        Console.WriteLine("Beitrag: 80 Euro");
+                    }
+                    else if (erwerbstätig == "n")
+                    {
+                        Console.WriteLine("Beitrag: 40 Euro");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe, versuchen Sie es erneut.");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Beitrag: 50 Euro");
+            }
+        }
+    }
+}
+
